b4f835f2719d5ef329e4bf8cd1d25a88dc8a59a8
Pedro Castilho <cstl.px@gmail.com>
2019-12-07 22:01:26 -0300

    Day 1 part 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,10 @@
 module Lib
-    ( totalFuelRequirement
+    ( totalFuelRequirements
     ) where
 
 import System.IO (isEOF)
 
+-- gets the mass of a single module from the input
 getModuleMass :: IO (Maybe Integer)
 getModuleMass = do
   done <- isEOF
@@ -11,16 +12,25 @@ getModuleMass = do
     then return Nothing
     else Just <$> read <$> getLine
 
-fuelRequirement :: Integer -> Integer
-fuelRequirement x = (x `div` 3) - 2
+-- gets the fuel a given mass would need if fuel had 0 mass
+naiveFuelRequirement :: Integer -> Integer
+naiveFuelRequirement mass = max 0 ((mass `div` 3) - 2)
 
-totalFuelRequirement :: IO Integer
-totalFuelRequirement = addFurtherRequirements 0
+-- gets the fuel a given mass *actually* needs, taking fuel weight into consideration
+moduleFuelRequirement :: Integer -> Integer
+moduleFuelRequirement mass =
+  moduleFuelRequirement' mass 0
+    where moduleFuelRequirement' residual acc =
+            let fuelMass = naiveFuelRequirement residual in
+              if fuelMass == 0
+              then acc
+              else moduleFuelRequirement' fuelMass (acc + fuelMass)
+
+-- adds up all fuel requirements for all the mass
+totalFuelRequirements :: IO Integer
+totalFuelRequirements = addFurtherRequirements 0
   where addFurtherRequirements acc =
           getModuleMass >>= 
             maybe
               (pure acc)
-              (\mass -> addFurtherRequirements $ acc + (fuelRequirement mass))
-
-main :: IO ()
-main = (show <$> totalFuelRequirement) >>= putStrLn
+              (\mass -> addFurtherRequirements $ acc + (moduleFuelRequirement mass))
